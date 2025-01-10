7429b2a6177d4c0bdc4c1f1c4478b846d2691a37
Md Siyam <mdsiyam9424@gmail.com>
2025-01-10 18:49:36 +0600

    get all users

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
         const cartCollection = client.db("bristoDB").collection("carts");
 
         // user Collection
+
+        app.get("/users", async(req, res)=>{
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
         app.post("/users", async(req, res)=>{
             const user = req.body;
 
